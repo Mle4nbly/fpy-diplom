b5b7a3361c23b4368cb2a03caa3a01c2a21a7ecc
mle4nbly <mle4nbly@gmail.com>
2026-02-03 06:03:53 +0700

    fix views 2

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import permissions
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Count, Sum
 from .models import User
